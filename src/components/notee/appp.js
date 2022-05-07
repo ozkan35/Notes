409d87e434246fe0bf2674485cc2482ec36fee9e
unknown <ozkanust35@gmail.com>
2022-05-07 03:12:13 +0300

    n1

---
+++ src/components/notee/appp.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+
+const Appp = () => {
+  return (
+    <Container>
+        <Row>
+            <Col md={4}></Col>
+            <Col md={4}></Col>
+
+        </Row>
+    </Container>
+  )
+}
+
+export default Appp
---
+++ src/components/notee/not-from.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFrom = () => {
+  return (
+    <div>NotFrom</div>
+  )
+}
+
+export default NotFrom
